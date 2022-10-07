84c08645f217d75d5be51cca63d7d5efe14d141a
Aliman1987 <114251255+Aliman1987@users.noreply.github.com>
2022-10-08 03:05:01 +0530

    HomeWork for Seminar 001 copleted

--- Seminar_001/Program.cs
+++ Seminar_001/Program.cs
@@ -58,3 +58,24 @@ else
     Console.WriteLine($" {n} -> No ");
 }
 */
+/*
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+int current = -n;
+if(n < 0)
+{
+    current = n;
+    n = -n;
+}
+while(count < n)
+{
+    Console.Write(count + ", ");
+    count = count +2;
+}
+*/
