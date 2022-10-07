3197ebbbf15d43f01654be7e6cca4553584d7643
Aliman1987 <114251255+Aliman1987@users.noreply.github.com>
2022-10-07 22:27:17 +0530

    Решена третья задача

--- Seminar_001/Program.cs
+++ Seminar_001/Program.cs
@@ -19,3 +19,42 @@ else
 Console.WriteLine("Largest number is: " + n2 + " smallest number is: " + n1 );
 }
 */
+
+/*//  Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+Console.Write("Input a first number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a third number ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int nmax = n1;
+
+if(n2 > nmax) nmax = n2;
+if(n3 > nmax) nmax = n3;
+
+Console.WriteLine($"Maximum number of numbers {n1}, {n2}, {n3} is {nmax} ");
+*/
+/*
+//  Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//  4 -> да
+//  -3 -> нет
+//  7 -> нет
+Console.Write("Input a number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n % 2 == 0)
+{
+    Console.WriteLine($" {n} -> Yes ");
+}
+else
+{
+    Console.WriteLine($" {n} -> No ");
+}
+*/
